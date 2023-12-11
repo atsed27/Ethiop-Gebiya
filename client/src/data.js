e34e7152fa3d845b9a1fd66634579003147793e4
atsed27 <danielnigatu09@gmail.com>
2023-12-11 05:20:11 -0300

    slider and catagory img is add

--- client/src/data.js
+++ client/src/data.js
@@ -5,21 +5,21 @@ import imgs4 from './img/da.jpg';
 export const slidersItems = [
   {
     id: 1,
-    img: imgs,
+    img: 'https://i.ibb.co/DG69bQ4/2.png',
     title: 'SUMMER SAL',
     desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS .",
     bg: 'f5fafd',
   },
   {
     id: 2,
-    img: imgs,
+    img: 'https://i.ibb.co/DG69bQ4/2.png',
     title: 'Daniel SAL',
     desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS .",
     bg: 'fcf1ed',
   },
   {
     id: 3,
-    img: imgs,
+    img: 'https://i.ibb.co/cXFnLLV/3.png',
     title: 'Lam SAL',
     desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS .",
     bg: 'fbf0f4',
@@ -29,19 +29,19 @@ export const slidersItems = [
 export const categories = [
   {
     id: 1,
-    img: imgs4,
+    img: 'https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     title: 'SHIRT STYLE',
     category: 'tishert',
   },
   {
     id: 2,
-    img: imgs3,
+    img: 'https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     title: 'Light STYLE',
     category: 'men',
   },
   {
-    id: 1,
-    img: imgs2,
+    id: 3,
+    img: 'https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
     title: 'Old STYLE',
     category: 'coat',
   },
