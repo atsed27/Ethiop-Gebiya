2ab6e29141f776ecca06aac99a3d00c7a6a2a81c
atsed27 <danielnigatu09@gmail.com>
2023-12-13 04:38:17 -0300

    amount is get from redux

--- client/src/page/Stripe.jsx
+++ client/src/page/Stripe.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
+
 const kEy =
   'pk_test_51OGNW1FH7zgKERAJkyDaNGlXVOei0qeT20URg24F8f2ynpSOvGIn1Jq6PZKOGKbjQ5yJ53QUhfa6E6Sm4c1AUiaZ00xfjavCU2';
 
 function Stripe() {
   const [stripeToken, setStripeToke] = useState(null);
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
   const onToken = (token) => {
     console.log(token);
     setStripeToke(token);
@@ -18,7 +22,7 @@ function Stripe() {
           'https://e-gebiya-k75e.onrender.com/api/pay/stripe',
           {
             tokenId: stripeToken.id,
-            amount: 5000,
+            amount: cart.total,
           }
         );
         console.log(res);
