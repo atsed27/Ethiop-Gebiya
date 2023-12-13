357c01acebf12a480180d5ac0bdbfd55075ef5d2
atsed27 <danielnigatu09@gmail.com>
2023-12-13 05:27:17 -0300

    add succus page update 3

--- client/src/page/Success.jsx
+++ client/src/page/Success.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Success() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div>
         <button className="px-3 py-2 bg-green-400 rounded-lg">
-          Successful
+          <Link to="/">Successful</Link>
         </button>
         <p>
           Your order being in processing . <br />
