a507d02afb4db381e7dd7474ec0153e3ff1dd396
atsed27 <danielnigatu09@gmail.com>
2023-12-13 06:28:05 -0300

    chapa pay integration start

--- Server/controllers/stripe.js
+++ Server/controllers/stripe.js
@@ -1,5 +1,5 @@
 import stripePackage from 'stripe';
-
+import request from 'request';
 const stripe = stripePackage(process.env.Stripe);
 
 export const StripePay = async (req, res, next) => {
@@ -20,5 +20,31 @@ export const StripePay = async (req, res, next) => {
 };
 
 export const chapaPay = async (req, res, next) => {
-  res.status(200).json('ok');
+  var options = {
+    method: 'POST',
+    url: 'https://api.chapa.co/v1/transaction/initialize',
+    headers: {
+      Authorization: process.env.chapaKey,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      amount: '100',
+      currency: 'ETB',
+      email: 'Danielnigatu09@gmail.com',
+      first_name: 'Daniel',
+      last_name: 'Nigatu',
+      phone_number: '0916213371',
+      tx_ref: 'chewatatest-21233',
+      callback_url: 'https://webhook.site/077164d6-29cb-40df-ba29-8a00e59a7e60',
+      return_url: 'https://www.google.com/',
+      'customization[title]': 'Payment for my favourite merchant',
+      'customization[description]': 'I love online payments',
+    }),
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+    const da = response.body;
+    res.status(200).json(response.body);
+  });
 };
