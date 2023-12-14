226066c3ea4b5f1d4d87a1f2e95d67da58eae591
atsed27 <danielnigatu09@gmail.com>
2023-12-14 05:01:44 -0300

    udate callback url

--- Server/controllers/stripe.js
+++ Server/controllers/stripe.js
@@ -35,7 +35,7 @@ export const chapaPay = async (req, res, next) => {
       last_name: 'Nigatu',
       phone_number: '0916213371',
       tx_ref: req.body.tx_ref,
-      callback_url: `https://e-gebiya-k75e.onrender.com/api/pay/chapa/${tx_ref}`,
+      callback_url: ``,
       return_url: 'https://e-gebiya.vercel.app/',
       'customization[title]': 'Payment for my favourite merchant',
       'customization[description]': 'I love online payments',
@@ -47,38 +47,4 @@ export const chapaPay = async (req, res, next) => {
   });
 };
 
-export const chapaPayVerify = async (req, res, next) => {
-  return new Promise((resolve, reject) => {
-    axios
-      .post('http://localhost:9401/api/chapa/da')
-      .then((response) => {
-        resolve(response.data);
-
-        console.log(response.data.response1);
-        var xx = response.data.response1;
-
-        var options = {
-          method: 'GET',
-          url: `https://api.chapa.co/v1/transaction/verify/${response.data.response1}`,
-          headers: {
-            Authorization:
-              'Bearer CHASECK_TEST-5fAt1GxUTLgcv5nuq7lRPvfe3QWqjwRM',
-          },
-        };
-        request(options, function (error, response) {
-          if (error) throw new Error(error);
-          console.log(response.body);
-          let yyy = response.body;
-          var responseData = {
-            xx,
-            yyy,
-          };
-          res.status(200).json(response.body);
-        });
-        //res.status(200).json(response.data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-};
+export const chapaPayVerify = async (req, res, next) => {};
